a34f521ac3f49e4365f8b3d4e862f46352abed9c
qianying guo <qyguo@buaa.edu.cn>
2023-05-16 12:20:33 +0200

    gem muon efficiency by TnP for offline

--- dqmgui/layouts/gem_T0_layouts.py
+++ dqmgui/layouts/gem_T0_layouts.py
@@ -72,3 +72,88 @@ GEMLayout(dqmitems, '12 GE11-P-L2 Chamber Efficiency',
 GEMLayout(dqmitems, '13 GE21-P-L2 DIGI Occupancy',
           [{'path': 'GEM/Digis/occ_GE21-P-L2',
             'description': _GEM_OFF_TWIKI}])
+
+
+GEMLayout(dqmitems, '14 GEM_chamberEff_chamber_1D', 
+    [{'path': 'GEM/Run summary/Segment_TnP/Task/GEM_chamberEff_chamber_1D', 'description': 'Chamber efficiency'}], 
+    )
+
+GEMLayout(dqmitems, '15 GEM_chamberEff_pt_1D', 
+    [{'path': 'GEM/Run summary/Segment_TnP/Task/GEM_chamberEff_pt_1D', 'description': 'pt efficiency'}], 
+    )
+
+GEMLayout(dqmitems, '16 GEM_chamberEff_eta_1D', 
+    [{'path': 'GEM/Run summary/Segment_TnP/Task/GEM_chamberEff_eta_1D', 'description': 'eta efficiency'}], 
+    )
+
+GEMLayout(dqmitems, '17 GEM_chamberEff_phi_1D', 
+    [{'path': 'GEM/Run summary/Segment_TnP/Task/GEM_chamberEff_phi_1D', 'description': 'phi efficiency'}], 
+    )
+
+GEMLayout(dqmitems, '18 GEM_chamberEff_chamber_p1_1D', 
+    [{'path': 'GEM/Run summary/Segment_TnP/Task/GEM_chamberEff_chamber_p1_1D', 'description': 'Chamber p1 efficiency'}], 
+    )
+
+GEMLayout(dqmitems, '19 GEM_chamberEff_chamber_p2_1D', 
+    [{'path': 'GEM/Run summary/Segment_TnP/Task/GEM_chamberEff_chamber_p2_1D', 'description': 'Chamber p2 efficiency'}], 
+    )
+
+GEMLayout(dqmitems, '20 GEM_chamberEff_chamber_n1_1D', 
+    [{'path': 'GEM/Run summary/Segment_TnP/Task/GEM_chamberEff_chamber_n1_1D', 'description': 'Chamber n1 efficiency'}], 
+    )
+
+GEMLayout(dqmitems, '21 GEM_chamberEff_chamber_n2_1D', 
+    [{'path': 'GEM/Run summary/Segment_TnP/Task/GEM_chamberEff_chamber_n2_1D', 'description': 'Chamber n2 efficiency'}], 
+    )
+
+GEMLayout(dqmitems, '22 GEM_chamberEff_pt_p1_1D', 
+    [{'path': 'GEM/Run summary/Segment_TnP/Task/GEM_chamberEff_pt_p1_1D', 'description': 'pt p1 efficiency'}], 
+    )
+
+GEMLayout(dqmitems, '23 GEM_chamberEff_pt_p2_1D', 
+    [{'path': 'GEM/Run summary/Segment_TnP/Task/GEM_chamberEff_pt_p2_1D', 'description': 'pt p2 efficiency'}], 
+    )
+
+GEMLayout(dqmitems, '24 GEM_chamberEff_pt_n1_1D', 
+    [{'path': 'GEM/Run summary/Segment_TnP/Task/GEM_chamberEff_pt_n1_1D', 'description': 'pt n1 efficiency'}], 
+    )
+
+GEMLayout(dqmitems, '25 GEM_chamberEff_pt_n2_1D', 
+    [{'path': 'GEM/Run summary/Segment_TnP/Task/GEM_chamberEff_pt_n2_1D', 'description': 'pt n2 efficiency'}], 
+    )
+
+GEMLayout(dqmitems, '26 GEM_chamberEff_eta_p1_1D', 
+    [{'path': 'GEM/Run summary/Segment_TnP/Task/GEM_chamberEff_eta_p1_1D', 'description': 'eta p1 efficiency'}], 
+    )
+
+GEMLayout(dqmitems, '27 GEM_chamberEff_eta_p2_1D', 
+    [{'path': 'GEM/Run summary/Segment_TnP/Task/GEM_chamberEff_eta_p2_1D', 'description': 'eta p2 efficiency'}], 
+    )
+
+GEMLayout(dqmitems, '28 GEM_chamberEff_eta_n1_1D', 
+    [{'path': 'GEM/Run summary/Segment_TnP/Task/GEM_chamberEff_eta_n1_1D', 'description': 'eta n1 efficiency'}], 
+    )
+
+GEMLayout(dqmitems, '29 GEM_chamberEff_eta_n2_1D', 
+    [{'path': 'GEM/Run summary/Segment_TnP/Task/GEM_chamberEff_eta_n2_1D', 'description': 'eta n2 efficiency'}], 
+    )
+
+GEMLayout(dqmitems, '30 GEM_chamberEff_phi_p1_1D', 
+    [{'path': 'GEM/Run summary/Segment_TnP/Task/GEM_chamberEff_phi_p1_1D', 'description': 'phi p1 efficiency'}], 
+    )
+
+GEMLayout(dqmitems, '31 GEM_chamberEff_phi_p2_1D', 
+    [{'path': 'GEM/Run summary/Segment_TnP/Task/GEM_chamberEff_phi_p2_1D', 'description': 'phi p2 efficiency'}], 
+    )
+
+GEMLayout(dqmitems, '32 GEM_chamberEff_phi_n1_1D', 
+    [{'path': 'GEM/Run summary/Segment_TnP/Task/GEM_chamberEff_phi_n1_1D', 'description': 'phi n1 efficiency'}], 
+    )
+
+GEMLayout(dqmitems, '33 GEM_chamberEff_phi_n2_1D', 
+    [{'path': 'GEM/Run summary/Segment_TnP/Task/GEM_chamberEff_phi_n2_1D', 'description': 'phi n2 efficiency'}], 
+    )
+
+GEMLayout(dqmitems, '34 GEM_chamberEff_Ch_region_GE1', 
+    [{'path': 'GEM/Run summary/Segment_TnP/Task/GEM_chamberEff_Ch_region_GE1', 'description': 'chamber VS region efficiency'}], 
+    )
